26d83a9e3370553e19bc1762fe72c4885f55266b
Jeffrey Ip <jeffreyip54@gmail.com>
2020-08-04 11:00:38 +0800

    this is the half finished landing and show page

---
+++ pages/landing/landing.js
@@ -0,0 +1,74 @@
+// pages/landing/landing.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    popUp: false,
+    blur: false
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  enablePopUp: function (e) {
+    this.setData({popUp: true, blur: true});
+    console.log(this.data)
+  },
+  hidePopUp: function() {
+  }
+})
